774cf3d1b72ddaa9bd52366a68ea3ccedf366260
Josh Kinlaw <jkinlaw@redhat.com>
2018-02-08 10:19:37 -0500

    adds searching and sorting to view-rhsa

--- app/js/components/vulnerabilities/vulnerabilitiesRhsaView/vulnerabilitiesRhsaView.controller.js
+++ app/js/components/vulnerabilities/vulnerabilitiesRhsaView/vulnerabilitiesRhsaView.controller.js
@@ -5,7 +5,9 @@ const componentsModule = require('../../');
 /**
  * @ngInject
  */
-function vulnerabilitiesRhsaViewCtrl($scope,
+function vulnerabilitiesRhsaViewCtrl($filter,
+                                     $location,
+                                     $scope,
                                      $stateParams,
                                      $state,
                                      InsightsConfig,
@@ -23,6 +25,13 @@ function vulnerabilitiesRhsaViewCtrl($scope,
     $scope.showSystem = InventoryService.showSystemModal;
     $scope.tabs = SystemModalTabs;
 
+    $scope.sorter = new Utils.Sorter(
+        {
+            predicate: $location.search().sort_by || 'toString',
+            reverse: $location.search().reverse || false
+        },
+        order);
+
     function initPageHeader () {
         const release_date = `Release Date: ${$scope.rhsa.issued}`;
         const package_count = $scope.rhsa.packages.length === 1 ? '1 Package' :
@@ -37,6 +46,12 @@ function vulnerabilitiesRhsaViewCtrl($scope,
     }
 
     function getData () {
+
+        // TODO: enable once API is available
+        // let params = {};
+        // params.sort_by = $scope.sorter.predicate;
+        // params.sort_dir = $scope.sorter.getSortDirection();
+
         Vulnerability.getRHSA($scope.rhsa_id).then((rhsa) => {
             $scope.rhsa = rhsa;
             breadcrumbs.add({
@@ -47,15 +62,33 @@ function vulnerabilitiesRhsaViewCtrl($scope,
                 }
             });
             initPageHeader();
-            $scope.totalSystems = rhsa.systems_affected;
+            $scope.affectedSystems = rhsa.systems;
         });
+    }
+
+    function order () {
+        $location.search('sort_by', $scope.sorter.predicate);
+        $location.search('reverse', $scope.sorter.reverse);
+
+        // TODO: use this once api is available
+        // getData();
+
+        $scope.affectedSystems = $filter('orderBy')(
+            $scope.affectedSystems,
+            [($scope.sorter.reverse ?
+                '-' + $scope.sorter.predicate :
+                $scope.sorter.predicate)]);
     }
 
     getData();
 
     $scope.search = function (model) {
-        //TODO: table search
-        console.log(model);
+        if (!model || model === '') {
+            $scope.affectedSystems = $scope.rhsa.systems;
+        } else {
+            $scope.affectedSystems = $filter('filter')(
+              $scope.rhsa.systems, model);
+        }
     };
 }
 
